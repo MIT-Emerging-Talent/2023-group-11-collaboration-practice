b3c992a7e69d3bb4d36f9b3b666f8fc703208dd2
YaninaChukanova <yana_chukanova@bigmir.net>
2024-01-21 22:53:51 +0100

    Correcting mistakes and adding documentation  in Quick_sort problem

--- src/sorting/python/quick_sort/src/quick_sort.py
+++ src/sorting/python/quick_sort/src/quick_sort.py
@@ -1,61 +1,104 @@
 import random
-# Time complexity O(nlogn)
-# in-place 2-way partition
-def quicksort_v1(array):
+from typing import List
+
+def quicksort_v1(array: List[int]) -> List[int]:
+    """
+    Sorts the input list in-place using the 2-way partition quicksort algorithm.
+
+    Parameters:
+    - array (List[int]): The list to be sorted.
+
+    Returns:
+    - List[int]: The sorted list.
+    """
     def _quicksort(array, start, stop):
-        # Base case: if the sublist has 1 or 0 elements, it's already sorted
+        """
+        Recursive function to perform 2-way partition quicksort.
+
+        Parameters:
+        - array (List[int]): The list to be sorted.
+        - start (int): Start index of the sublist.
+        - stop (int): Stop index of the sublist.
+
+        Returns:
+        - None
+        """
         if start < stop - 1:
-           # Choose a random pivot and partition the sublist 
-            pivot_index = partition(array, start, stop) 
-             # Recursively sort the sublists on both sides of the pivot
+            pivot_index = partition(array, start, stop)
             _quicksort(array, start, pivot_index)
             _quicksort(array, pivot_index + 1, stop)
 
     def partition(array, start, stop):
-        # Randomly choose a pivot and move it to the end of the sublist
+        """
+        Perform 2-way partitioning and return the index of the pivot.
+
+        Parameters:
+        - array (List[int]): The list to be partitioned.
+        - start (int): Start index of the sublist.
+        - stop (int): Stop index of the sublist.
+
+        Returns:
+        - int: The index of the pivot.
+        """
         pivot_index = random.randint(start, stop - 1)
         array[pivot_index], array[stop - 1] = array[stop - 1], array[pivot_index]
         pivot = array[stop - 1]
 
-        # Initialize the pointer 'i' at the start of the sublist
         i = start
-        # Iterate through the sublist and move elements smaller than the pivot to the left
         for j in range(start, stop - 1):
             if array[j] <= pivot:
                 array[i], array[j] = array[j], array[i]
                 i += 1
 
-        # Iterate through the sublist and move elements smaller than the pivot to the left
-
         array[i], array[stop - 1] = array[stop - 1], array[i]
         return i
-     # Call the recursive quicksort function on the entire array
+
     _quicksort(array, 0, len(array))
     return array
 
 
-# Time complexity O(nlogn)
-# in-place 3-way partition
-def quicksort_v2(array):
-    # Base case: if the sublist has 1 or 0 elements, it's already sorted
-    def _quicksort(array, start, stop):
+def quicksort_v2(array: List[int]) -> List[int]:
+    """
+    Sorts the input list in-place using the 3-way partition quicksort algorithm.
 
+    Parameters:
+    - array (List[int]): The list to be sorted.
+
+    Returns:
+    - List[int]: The sorted list.
+    """
+    def _quicksort(start: int, stop: int):
+        """
+        Recursive function to perform 3-way partition quicksort.
+
+        Parameters:
+        - start (int): Start index of the sublist.
+        - stop (int): Stop index of the sublist.
+
+        Returns:
+        - None
+        """
         if stop - start < 2:
             return
-        # Perform 3-way partitioning and get the indices of the equal elements range
-        lt, gt = partition(array, start, stop)
-        #Recursively sort the sublists less than the pivot and greater than the pivot
-        _quicksort(array, start, lt)
-        _quicksort(array, gt, stop)
+        lt, gt = partition(start, stop)
+        _quicksort(start, lt)
+        _quicksort(gt, stop)
 
-    def partition(array, start, stop):
-        # Choose a random pivot and initialize pointers
+    def partition(start: int, stop: int):
+        """
+        Perform 3-way partitioning and return the indices of the equal elements range.
+
+        Parameters:
+        - start (int): Start index of the sublist.
+        - stop (int): Stop index of the sublist.
+
+        Returns:
+        - Tuple[int, int]: Indices of the equal elements range.
+        """
         pivot_index = random.randint(start, stop - 1)
         pivot = array[pivot_index]
 
-        # Initialize pointers
         lt, i, gt = start, start, stop
-        # Iterate through the sublist and rearrange elements based on their relationship to the pivot
         while i < gt:
             if array[i] < pivot:
                 array[i], array[lt] = array[lt], array[i]
@@ -66,8 +109,7 @@ def quicksort_v2(array):
                 array[i], array[gt] = array[gt], array[i]
             else:
                 i += 1
-        # Return the indices marking the range of equal elements
         return lt, gt
-    # Call the recursive quicksort function on the entire array
-    _quicksort(array, 0, len(array))
+
+    _quicksort(0, len(array))
     return array
